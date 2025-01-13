c9473a1e98802fb9859a0eaf6d1df4e67708b2dc
Trevor Deyo <tdeyo50@gmail.com>
2025-01-12 20:55:26 -0600

    switched to phi4

--- DeepSeek/gunicorn_config.py
+++
@@ -1,8 +0,0 @@
-import multiprocessing
-
-# Calculate the number of workers based on the number of CPU cores
-workers = min((multiprocessing.cpu_count() * 2) + 1, 2)  # Limit to a maximum of 2 workers
-
-# Gunicorn configuration
-bind = "0.0.0.0:5000"
-timeout = 120
